d989d81c3484aafe215fd0ac1ffe3a41b61cf35b
Youssef George <134179178+YoussefGeorge-Dev@users.noreply.github.com>
2026-04-18 12:21:46 +0200

    solved q1

--- Introduction-to-Programming/Lab05/Lab05.cpp
+++ Introduction-to-Programming/Lab05/Lab05.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+// * Question 1 
+
+int main() 
+{
+ char first = 'A';
+ char second = 'B';
+ char* ptr1 = &first;
+ char* ptr2 = &second;
+ *ptr1 = 'C';
+ *ptr2 = 'D';
+ cout << "first: " << first << endl;
+ cout << "first (via pointer): " << *ptr1 << endl;
+ cout << "second: " << second << endl;
+ cout << "second (via pointer): " << *ptr2 << endl;
+ return 0;
+}
